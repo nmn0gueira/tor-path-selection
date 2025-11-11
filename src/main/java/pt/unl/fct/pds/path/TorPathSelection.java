58c1104ae824d99914ecd04c2408dc47c1efec0b
Nuno Nogueira <101476982+nmn0gueira@users.noreply.github.com>
2025-11-11 23:39:35 +0000

    Add subnet filtering

--- src/main/java/pt/unl/fct/pds/path/TorPathSelection.java
+++ src/main/java/pt/unl/fct/pds/path/TorPathSelection.java
@@ -1,6 +1,8 @@
 package pt.unl.fct.pds.path;
 
 import pt.unl.fct.pds.model.Node;
+import pt.unl.fct.pds.utils.GeneralUtils;
+import pt.unl.fct.pds.utils.RandomCollection;
 
 import java.util.List;
 
@@ -13,11 +15,35 @@ public class TorPathSelection extends AbstractPathSelection{
 
     @Override
     public Node getGuardNode(Node exitNode) {
-        return null;
+        RandomCollection<Node> suitableNodes = new RandomCollection<>();
+        for  (Node node : nodes) {
+            if (!node.getFlags().contains("Guard"))
+                continue;
+
+            // TODO: Also filter nodes of the same family
+            if (GeneralUtils.same16Subnet(node.getIpAddress(), exitNode.getIpAddress()))
+                continue;
+
+            // TODO: Prioritize relays from persistent SAMPLED_GUARDS and CONFIRMED_GUARDS sets ??????
+            suitableNodes.add(node.getBandwidth(), node);
+        }
+        return suitableNodes.next();
     }
 
     @Override
     public Node getMiddleNode(Node guardNode, Node exitNode) {
-        return null;
+        RandomCollection<Node> suitableNodes = new RandomCollection<>();
+        for  (Node node : nodes) {
+            if (!node.getFlags().contains("Fast"))
+                continue;
+            // Might not need to verify both addresses since the subnet for both the guard and exit should be the same already
+            // TODO: Also filter nodes of the same family
+            if (GeneralUtils.same16Subnet(node.getIpAddress(), exitNode.getIpAddress())
+            && GeneralUtils.same16Subnet(node.getIpAddress(), guardNode.getIpAddress()))
+                continue;
+
+            suitableNodes.add(node.getBandwidth(), node);
+        }
+        return suitableNodes.next();
     }
 }
